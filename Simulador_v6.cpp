427490a32dbf7a3e57719cbfa3cad121fd370940
Leopoldo Morante <98835751+Leo11wasd@users.noreply.github.com>
2023-12-14 20:21:56 -0600

    Update Simulador_v6.cpp
    Con luz del sol y todo xd. Así no rota la tierra xdd

--- Simulador_v6.cpp
+++ Simulador_v6.cpp
@@ -77,6 +77,12 @@ VEC3 vel_2 = {0.0, 946.6322, 0.0};
 
 VEC3 pos_3 = {95.786E6, 0.0, 0.0}; //35.786E6
 VEC3 vel_3 = {0.0, 2E3, 0.0};
+vec4 elcolor;
+vec4 ambiental;
+vec4 specular_mat;
+vec4 ambiental_mat;
+vec4 difusa_mat;
+float shinyness;
 
 double deltaT = 0.01 * 3600; // segundos en una hora
 //double t_horas = 0.0f;
@@ -110,33 +116,76 @@ const unsigned int SCR_WIDTH = 800;
 const unsigned int SCR_HEIGHT = 600;
 
 
-const char *vertexShaderSource ="#version 330 core\n"
+const char *vertexShaderSource = "#version 330 core\n"
     "uniform mat4 MVP;\n"
+    "uniform mat4 MW;\n"
     "layout (location = 0) in vec3 aPos;\n"
-    "layout (location = 1) in vec3 aCol;\n"
+    "layout (location = 1) in vec3 aColor;\n"
     "layout (location = 2) in vec2 aTexCoord;\n"
-    "out vec3 color;\n"
+    "layout (location = 3) in vec3 aNorm;\n"
+    "out vec3 ourColor;\n"
     "out vec2 TexCoord;\n"
+    "out vec3 Normales;\n"
+    "out vec3 Posicion;\n"
+    "vec4 pospre;\n"
+    "vec4 normpre;\n"
     "void main()\n"
     "{\n"
-    "   gl_Position = MVP * vec4(aPos.x,aPos.y,aPos.z, 1.0);\n"
-    "   color = aCol;\n"
-    "   TexCoord = vec2(aTexCoord.x, aTexCoord.y);\n"
-    "}\0";
+    "	gl_Position = MVP * vec4(aPos.x, aPos.y, aPos.z, 1.0);\n"
+    "   pospre = MW * vec4(aPos.x, aPos.y, aPos.z, 1.0);\n"
+    "   Posicion= vec3(pospre.x,pospre.y,pospre.z);\n"
+    "	ourColor = aColor;\n"
+    "	TexCoord = vec2(aTexCoord.x, aTexCoord.y);\n"
+    "   normpre = MW * vec4(aNorm.x,aNorm.y,aNorm.z,0.0);\n"
+    "   Normales= vec3(normpre.x,normpre.y,normpre.z);\n"
+    "}\n\0";
 
 const char *fragmentShaderSource = "#version 330 core\n"
     "out vec4 FragColor;\n"
-    "in vec3 color;\n"
+    "in vec3 ourColor;\n"
     "in vec2 TexCoord;\n"
-
+    "in vec3 Normales;\n"
+    "in vec3 Posicion;\n"
+    "// texture sampler\n"
     "uniform sampler2D texture1;\n"
+    //"uniform vec4 ambiental;\n"
+    "uniform vec3 Luzpos;\n"
+    "uniform vec4 colorLuz;\n"
+    "uniform vec3 posCamara;\n"
+
+    "uniform float shinyness_mat;\n" //"shininess" del material
+    "uniform vec4 specular_mat;\n" //specular del material
+    "uniform vec4 difusa_mat;\n" //difusa del material
+    "uniform vec4 ambiental_mat;\n" //ambiental del material
+
+    "vec4 color_frag;\n"
     "void main()\n"
     "{\n"
-    "     FragColor = texture(texture1, TexCoord);\n"
+
+    "float specularStrength=0.7;\n"
+    "vec3 norm=normalize(Normales);\n"
+    "vec3 lightDir= normalize(Luzpos-Posicion);\n"
+	"float diff = max(dot(norm, lightDir), 0.0);\n"
+    "vec4 Luzdifusa = diff * colorLuz * difusa_mat;\n"
+
+    "vec3 viewDir = normalize(posCamara - Posicion);\n"
+    "vec3 reflectDir = reflect(-lightDir, norm);\n"
+    "float spec = pow(max(dot(viewDir, reflectDir), 0.0), shinyness_mat);\n"
+    "if(dot(reflectDir,norm)<0.0) spec = 0.0;"
+    "vec4 specular = specularStrength * spec * colorLuz * specular_mat;\n"
+//    "ambiental = ambiental * ambiental_mat;\n"
+
+	"     color_frag = texture(texture1, TexCoord);\n"
+	"     FragColor = vec4(color_frag*(Luzdifusa+ambiental_mat+specular));\n"
     "}\n\0";
 
 int main()
 {
+
+    elcolor[0] = 1.f;
+    elcolor[1] = 1.f;
+    elcolor[2] = 1.0f;
+    elcolor[3] = 0.8f;
 //////////////////////////////////////////////////////////////////
 
 //////////////////////////////////////////////////////////////////
@@ -160,7 +209,10 @@ int main()
 //////////////////////////////////////////////////////////////////
 
     float x,y,z;
-    GLint mvp_location, vpos_location, vcol_location;
+        GLint mvp_location,posCamara_location,mw_location, mvp_ejes_location, mvp_cuboLuz_location,
+          el_color_location, vpos_location, vcol_location,ambiental_location, luzpos_location,
+          colorluz_location,specular_mat_location,shiny_mat_location,ambiental_mat_location,difusa_mat_location;
+
 
     // glfw: initialize and configure
     // ------------------------------
@@ -234,6 +286,15 @@ int main()
     }
 
     mvp_location = glGetUniformLocation(shaderProgram, "MVP");
+    mw_location = glGetUniformLocation(shaderProgram, "MW");
+    ambiental_location =glGetUniformLocation(shaderProgram, "ambiental");
+    luzpos_location=glGetUniformLocation(shaderProgram,"Luzpos");
+    posCamara_location=glGetUniformLocation(shaderProgram,"posCamara");
+    colorluz_location=glGetUniformLocation(shaderProgram,"colorLuz");
+    specular_mat_location=glGetUniformLocation(shaderProgram,"specular_mat");
+    ambiental_mat_location=glGetUniformLocation(shaderProgram,"ambiental_mat");
+    difusa_mat_location=glGetUniformLocation(shaderProgram,"difusa_mat");
+    shiny_mat_location=glGetUniformLocation(shaderProgram,"shinyness_mat");
     glDeleteShader(vertexShader);
     glDeleteShader(fragmentShader);
 
@@ -299,6 +360,8 @@ int main()
 
       glVertexAttribPointer(2, 2, GL_FLOAT, GL_FALSE, sizeof(VERTICE_TEX_NOR), (void*)(6 * sizeof(float)));
       glEnableVertexAttribArray(2);
+      glVertexAttribPointer(3, 3, GL_FLOAT, GL_FALSE, sizeof(VERTICE_TEX_NOR), (void*)(8 * sizeof(float)));
+      glEnableVertexAttribArray(3);
 
       glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, EBO_tierra);
       glBufferData(GL_ELEMENT_ARRAY_BUFFER, num_indices_tierra * sizeof(unsigned int), indices_tierra, GL_STATIC_DRAW);
@@ -333,6 +396,8 @@ int main()
 
       glVertexAttribPointer(2, 2, GL_FLOAT, GL_FALSE, sizeof(VERTICE_TEX_NOR), (void*)(6 * sizeof(float)));
       glEnableVertexAttribArray(2);
+      glVertexAttribPointer(3, 3, GL_FLOAT, GL_FALSE, sizeof(VERTICE_TEX_NOR), (void*)(8 * sizeof(float)));
+      glEnableVertexAttribArray(3);
 
 
       glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, EBO_luna);
@@ -349,7 +414,7 @@ int main()
 
 unsigned int VBO_sat, VAO_sat, EBO_sat;
 
-    //if(TIPO == PRISMA)
+
     {
       glGenVertexArrays(1, &VAO_sat);
       glGenBuffers(1, &VBO_sat);
@@ -369,6 +434,8 @@ unsigned int VBO_sat, VAO_sat, EBO_sat;
 
       glVertexAttribPointer(2, 2, GL_FLOAT, GL_FALSE, sizeof(VERTICE_TEX_NOR), (void*)(6 * sizeof(float)));
       glEnableVertexAttribArray(2);
+      glVertexAttribPointer(3, 3, GL_FLOAT, GL_FALSE, sizeof(VERTICE_TEX_NOR), (void*)(8 * sizeof(float)));
+      glEnableVertexAttribArray(3);
 
 
       glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, EBO_sat);
@@ -468,6 +535,21 @@ unsigned int VBO_sat, VAO_sat, EBO_sat;
     float deltapos=0.001;
     while (!glfwWindowShouldClose(window))
     {
+            shinyness= 0.5;
+            ambiental_mat[0]=0.4;
+            ambiental_mat[1]=0.4;
+            ambiental_mat[2]=0.4;
+
+            difusa_mat[0]=0.9;
+            difusa_mat[1]=0.9;
+            difusa_mat[2]=0.9;
+
+            specular_mat[0]=0.1;
+            specular_mat[1]=0.1;
+            specular_mat[2]=0.1;
+
+
+
         // input
         // -----
         processInput(window);
@@ -482,13 +564,17 @@ unsigned int VBO_sat, VAO_sat, EBO_sat;
 
 		float ratio;
 		int width, height;
-		mat4x4 mcam, p, mvp,pers,mk,mtr,m;
+		mat4x4 mcam, p, mvp,pers,mk,mtr,m,mw;
 		mat4x4_perspective(pers,30.0*atan(1.0)/45.0,800.0/600.0, 1.0,40.0);
 
 		glfwGetFramebufferSize(window, &width, &height);
 		ratio = width / (float)height;
 
 		glViewport(0, 0, width, height);
+        float posluz[3];
+        posluz[0]=1110.0f;
+        posluz[1]=1110.0f;
+        posluz[2]=-11110.5f;
 
 for(int t= 0; t < 3; t++)
     //2 -> SATELITE
@@ -564,9 +650,8 @@ for(int t= 0; t < 3; t++)
                 target[2]+=deltaz;
                 }
 
-		eye[0] = target[0] ;
-        eye[1] = target[1] ;
-		eye[2] = -6;//-6
+
+		eye[2] = -5;//-6
 
 
         mat4x4_look_at( mcam, eye, target, up );
@@ -575,6 +660,7 @@ for(int t= 0; t < 3; t++)
 		  mat4x4_translate(mtr,x,y,z);
 
 		  mat4x4_mul(mk, mtr, mk);
+          mat4x4_dup(mw,mk);
           mat4x4_identity(mvp);
           mat4x4_mul(mvp,mcam,mk);
           mat4x4_mul(mvp,pers,mvp);
@@ -586,6 +672,17 @@ for(int t= 0; t < 3; t++)
             num_indices=num_indices_tierra;
               usar_textura = texture1;
               glBindTexture(GL_TEXTURE_2D, usar_textura);
+              glUniformMatrix4fv(mw_location, 1, GL_FALSE, (const GLfloat*)mw);
+              glUniformMatrix4fv(mvp_location, 1, GL_FALSE, (const GLfloat*)mvp);
+              //+glUniform4fv(ambiental_location, 1, (const GLfloat*)ambiental);
+              glUniform4fv(luzpos_location,1,(const GLfloat*) posluz);
+              glUniform4fv(posCamara_location,1,(const GLfloat*)eye);
+              glUniform4fv(colorluz_location,1,(const GLfloat*)elcolor);
+              glUniform4fv(specular_mat_location,1,(const GLfloat*)specular_mat);
+              glUniform4fv(ambiental_mat_location,1,(const GLfloat*)ambiental_mat);
+              glUniform4fv(difusa_mat_location,1,(const GLfloat*)difusa_mat);
+              glUniform1f(shiny_mat_location,shinyness);
+
               glBindVertexArray(VAO_tierra);
           }
           if(t==1)
@@ -593,6 +690,17 @@ for(int t= 0; t < 3; t++)
               num_indices=num_indices_luna;
               usar_textura = texture2;
               glBindTexture(GL_TEXTURE_2D, usar_textura);
+              glUniformMatrix4fv(mw_location, 1, GL_FALSE, (const GLfloat*)mw);
+              glUniformMatrix4fv(mvp_location, 1, GL_FALSE, (const GLfloat*)mvp);
+              //+glUniform4fv(ambiental_location, 1, (const GLfloat*)ambiental);
+              glUniform4fv(luzpos_location,1,(const GLfloat*) posluz);
+              glUniform4fv(posCamara_location,1,(const GLfloat*)eye);
+              glUniform4fv(colorluz_location,1,(const GLfloat*)elcolor);
+              glUniform4fv(specular_mat_location,1,(const GLfloat*)specular_mat);
+              glUniform4fv(ambiental_mat_location,1,(const GLfloat*)ambiental_mat);
+              glUniform4fv(difusa_mat_location,1,(const GLfloat*)difusa_mat);
+              glUniform1f(shiny_mat_location,shinyness);
+
               glBindVertexArray(VAO_luna);
 
               }
@@ -603,6 +711,17 @@ for(int t= 0; t < 3; t++)
               num_indices=num_indices_sat;
               usar_textura = texture3;
               glBindTexture(GL_TEXTURE_2D, usar_textura);
+                glUniformMatrix4fv(mw_location, 1, GL_FALSE, (const GLfloat*)mw);
+              glUniformMatrix4fv(mvp_location, 1, GL_FALSE, (const GLfloat*)mvp);
+              //+glUniform4fv(ambiental_location, 1, (const GLfloat*)ambiental);
+              glUniform4fv(luzpos_location,1,(const GLfloat*) posluz);
+              glUniform4fv(posCamara_location,1,(const GLfloat*)eye);
+              glUniform4fv(colorluz_location,1,(const GLfloat*)elcolor);
+              glUniform4fv(specular_mat_location,1,(const GLfloat*)specular_mat);
+              glUniform4fv(ambiental_mat_location,1,(const GLfloat*)ambiental_mat);
+              glUniform4fv(difusa_mat_location,1,(const GLfloat*)difusa_mat);
+              glUniform1f(shiny_mat_location,shinyness);
+
               glBindVertexArray(VAO_sat);
 
               }
